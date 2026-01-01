ed67472f05e453abf43851379639fdba3d57f8cf
DNS <37093+DNS@users.noreply.github.com>
2026-01-02 01:54:30 +0700

    benchmark

--- build-benchmark.ps1
+++ build-benchmark.ps1
@@ -5,10 +5,8 @@ gcc -O3 benchmark.c -o benchmark-c.out
 
 # benchmark, find fastest time
 
-$c,$r = @(100,100)
-
-$c = $(Measure-Command { ./benchmark-c.out }).TotalSeconds
-$r = $(Measure-Command { ./benchmark-rust.out }).TotalSeconds 
+$c = Measure-Command { ./benchmark-c.out }
+$r = Measure-Command { ./benchmark-rust.out }
 
 
 ri *.out -Force
