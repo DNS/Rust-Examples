b22af9daf27af5708ebe5586265744dcc7d6a2a4
DNS <37093+DNS@users.noreply.github.com>
2026-01-02 01:53:24 +0700

    benchmark

--- build-benchmark.ps1
+++ build-benchmark.ps1
@@ -1,8 +1,15 @@
 rustc -C opt-level=3 -C strip=symbols benchmark.rs -o benchmark-rust.out
-gcc -O3 ./benchmark.c -o benchmark-c.out
 
-$c = Measure-Command { ./benchmark-c.out } 
-$r = Measure-Command { ./benchmark-rust.out } 
+gcc -O3 benchmark.c -o benchmark-c.out
+
+
+# benchmark, find fastest time
+
+$c,$r = @(100,100)
+
+$c = $(Measure-Command { ./benchmark-c.out }).TotalSeconds
+$r = $(Measure-Command { ./benchmark-rust.out }).TotalSeconds 
+
 
 ri *.out -Force
 
