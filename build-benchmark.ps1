5d59562d59583cac0be7bb116aa99a3fa91b6031
DNS <37093+DNS@users.noreply.github.com>
2026-01-02 01:52:30 +0700

    benchmark

---
+++ build-benchmark.ps1
@@ -0,0 +1,11 @@
+rustc -C opt-level=3 -C strip=symbols benchmark.rs -o benchmark-rust.out
+gcc -O3 ./benchmark.c -o benchmark-c.out
+
+$c = Measure-Command { ./benchmark-c.out } 
+$r = Measure-Command { ./benchmark-rust.out } 
+
+ri *.out -Force
+
+'Total Seconds'
+"C    : $($c.TotalSeconds)"
+"Rust : $($r.TotalSeconds)"
